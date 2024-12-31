a8a5429bc226d62a4497fc61d5dc5cdea7b6db08
logesh <logeshspltm@gmail.com>
2024-12-31 16:27:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ def ai(txt):
         if 'generateContent' in m.supported_generation_methods:
             print(m.name)
     model = genai.GenerativeModel('gemini-pro')
-    response = model.generate_content("response in simple manner"+txt)
+    response = model.generate_content("give detailed information "+txt)
     return response.text
 
 
